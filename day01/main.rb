54b24d2742710cbf357829c520398a2040e4d23d
Mike Errington <mike.errington@gmail.com>
2021-12-01 08:46:16 -0500

    Cleanup - finish editor setup, rubocop

--- day01/main.rb
+++ day01/main.rb
@@ -1,46 +1,41 @@
 INPUTS = {
-    sample: 'sample_in.txt',
-    default: 'in.txt',
-}
+  sample: 'sample_in.txt',
+  default: 'in.txt',
+}.freeze
 
 def input(path)
-    File.read(path).split
+  File.read(path).split
 end
 
-
 def part1(input)
-    initial = {
-        count: 0,
-        prev: nil
-    }
-    input.reduce(initial) do |memo, obj|
-        prev = memo[:prev]
-        memo[:prev] = obj.to_i
+  initial = {
+    count: 0,
+    prev: nil,
+  }
+  input.each_with_object(initial) do |obj, memo|
+    prev = memo[:prev]
+    memo[:prev] = obj.to_i
 
-        next memo if !prev
+    next memo unless prev
 
-        memo[:count] += 1 if prev < obj.to_i
-        memo
-    end
+    memo[:count] += 1 if prev < obj.to_i
+  end
 end
 
 def part2(input)
-    initial = {
-        list: [],
-        acc: [],
-    }
-    summed = input.reduce(initial) do |memo, obj|
-        # puts obj
-        memo[:acc] << obj.to_i
-        next memo unless memo[:acc].size % 3 == 0
+  initial = {
+    list: [],
+    acc: [],
+  }
+  summed = input.each_with_object(initial) do |obj, memo|
+    memo[:acc] << obj.to_i
+    next memo unless (memo[:acc].size % 3).zero?
 
-        memo[:list] << memo[:acc].sum
-        memo[:acc].shift
+    memo[:list] << memo[:acc].sum
+    memo[:acc].shift
+  end
 
-        memo
-    end
-
-    part1(summed[:list])
+  part1(summed[:list])
 end
 
 in_file = INPUTS[ARGV[0]&.to_sym || :default]
